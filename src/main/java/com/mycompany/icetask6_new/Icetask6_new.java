82f61f57af51a729aca8109fd10603e104fb076d
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-10-05 15:57:24 +0000

    first commit

--- src/main/java/com/mycompany/icetask6_new/Icetask6_new.java
+++ src/main/java/com/mycompany/icetask6_new/Icetask6_new.java
@@ -11,6 +11,25 @@ import java.util.Scanner;
  */
 public class Icetask6_new {
 
+    public class VowelCounter {
+    
+    // Function to count vowels
+    public static int countVowels(String sentence) {
+        // Convert the sentence to lowercase
+        sentence = sentence.toLowerCase();
+        // Variable to store the number of vowels
+        int vowelCount = 0;
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
