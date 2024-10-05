2813742b5d3e333ebaeea82688714628e1fe18ec
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-10-05 15:51:05 +0000

    first commit

---
+++ src/main/java/com/mycompany/icetask6_new/Icetask6_new.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask6_new;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Icetask6_new {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
